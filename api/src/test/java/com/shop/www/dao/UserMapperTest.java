ae0f1f89086ab6dbf9f7f51c9e6849ce09eb6293
1245193880 <1245193880@qq.com>
2017-04-06 23:44:44 +0800

    update

---
+++ api/src/test/java/com/shop/www/dao/UserMapperTest.java
@@ -0,0 +1,44 @@
+package com.shop.www.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring-*.xml")
+public class UserMapperTest {
+
+	@Test
+	public void testDeleteByPrimaryKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertSelective() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectByPrimaryKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateByPrimaryKeySelective() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateByPrimaryKey() {
+		fail("Not yet implemented");
+	}
+
+}
