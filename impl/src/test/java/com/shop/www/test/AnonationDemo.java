75173d186b20ec641d620716036fcd2ddb1f7932
1245193880 <1245193880@qq.com>
2017-06-03 23:28:14 +0800

    unfinished

---
+++ impl/src/test/java/com/shop/www/test/AnonationDemo.java
@@ -0,0 +1,11 @@
+package com.shop.www.test;
+
+import com.test.www.util.TTAnotation;
+
+public class AnonationDemo {
+
+	@TTAnotation(time="this is a time",value="this is a value")
+	public void anonation(){
+		System.out.println("this is a anonation use");
+	}
+}
---
+++ util/src/main/java/com/test/www/util/TTAnonationInterceptor.java
@@ -0,0 +1,21 @@
+package com.test.www.util;
+
+import java.lang.reflect.Method;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.util.ClassUtils;
+
+public class TTAnonationInterceptor implements MethodInterceptor {
+
+	@Override
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		// TODO Auto-generated method stub
+		Method method=ClassUtils.getMostSpecificMethod(invocation.getMethod(),invocation.getThis().getClass());
+		TTAnotation ttAnonation=method.getAnnotation(TTAnotation.class);
+		System.out.println("time is:"+ttAnonation.time()+""+"\nvalue is:"+ttAnonation.value());
+		return null;
+	}
+
+
+}
